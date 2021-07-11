ca32494a06c42326a3c1696c34c7ecc0c98c2f9e
Misaal <makstereb@gmail.com>
2021-07-11 13:02:25 +0300

    Change rainbow animation and favicon

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ db = Database()
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/images'), 'icon.png',
+    return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.ico',
                                mimetype='image/vnd.microsoft.icon')
 
 
